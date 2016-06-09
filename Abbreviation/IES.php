2f8286ccaaea61b9727ea6a7385e88e6ba0bef71
Almino Melo <almino.melo@gmail.com>
2016-06-09 16:16:43 -0300

    Mudança de nome.

--- Abbreviation/IES.php
+++ Abbreviation/IES.php
@@ -2,7 +2,7 @@
 
 namespace Onimla\Brazil\Abbreviation;
 
-class InstEnsinoSuperior extends \Onimla\HTML\Abbreviation {
+class IES extends \Onimla\HTML\Abbreviation {
 
     public function __construct() {
         parent::__construct('IES', 'Instituição de Ensino Superior');
