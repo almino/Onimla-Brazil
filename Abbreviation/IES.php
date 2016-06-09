1f0370bee279affebd870ae4c884360a084d871b
Almino Melo <almino.melo@gmail.com>
2016-06-09 13:53:57 -0300

    Instituição de Ensino Superior

---
+++ Abbreviation/IES.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Onimla\Brazil\Abbreviation;
+
+class IES extends \Onimla\HTML\Abbreviation {
+
+    public function __construct() {
+        parent::__construct('IES', 'Instituição de Ensino Superior');
+    }
+
+}
